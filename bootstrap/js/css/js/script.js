88e087a7e76e0cf02731d466f9c316f58a26a5f7
carmennelly <lesner082016@gmail.com>
2021-05-23 20:35:17 -0500

    primer commit

---
+++ bootstrap/js/css/js/script.js
@@ -0,0 +1,13 @@
+alert("educaciòn en casa");
+
+
+function educaciònencasa() {
+    var educaciònencasa = prompt ("la familia es clave en la educaciòn");
+    if (educaciònencasa == "si") {
+       alert("el nombre es correcto");
+    } else {
+        alert("el nombre es el incorrecto");
+    }  
+
+    
+}
